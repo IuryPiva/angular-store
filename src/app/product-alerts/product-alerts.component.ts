0965f8ab67d1642f1d8195e8cc815122a5c67dc0
Iury Piva <iurypiva@gmail.com>
2024-01-24 14:41:44 -0300

    Refactor product-alerts and product-list components

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Product } from "../products";
 
 @Component({
-  selector: 'app-product-alerts',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-alerts.component.html',
-  styleUrl: './product-alerts.component.css'
+  selector: "app-product-alerts",
+  templateUrl: "./product-alerts.component.html",
+  styleUrl: "./product-alerts.component.css",
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
-
+  @Output() notify = new EventEmitter();
 }
